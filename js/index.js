541465fe742bb33e196d53214c4cb6d2eed24731
Mark Rogowsky <65688548+markrogo@users.noreply.github.com>
2020-07-28 18:44:26 -0700

    page nearly matches example

--- js/index.js
+++ js/index.js
@@ -38,5 +38,84 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
+
+
+//rebuild navbar
+let navNL = document.querySelectorAll('nav a');
+
+let anchors = Object.values(siteContent.nav);
+anchors.pop();
+
+for (i=0; i < anchors.length; i++) {
+  console.log (anchors[i]);
+  navNL[i].textContent = anchors[i];
+  navNL[i].style.color = 'green';
+};
+
+// can we have an index in our for each??
+// anchors.forEach(e => { 
+//   navNL[e].textContent = anchors[e];
+// });
+
+
+// here's where we select the nave and then prepend and append
+
+// thus isnt code yet document.prependChild()
+
+
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+// rebuild middle secion
+const head = document.querySelector('h1');
+let newH1Str = (siteContent["cta"]["h1"]);
+
+
+
+head.textContent = (newH1Str);
+
+let buttonText = document.querySelector('.cta button');
+buttonText.textContent = (siteContent["cta"]["button"]);
+
+let codeImg = document.getElementById("cta-img");
+codeImg.setAttribute('src', siteContent["cta"]["img-src"]);
+
+//rebuild texty section
+let blocks = Object.values(siteContent["main-content"]);
+blocks.splice(4,1); // take out image and give array on heads and text blocks
+
+let headsNL = document.querySelectorAll(".text-content h4");
+let textNL = document.querySelectorAll(".text-content p");
+const headsArray = blocks.filter (function (element, index) {
+  return (index % 2 === 0);
+});
+const textArray = blocks.filter (function (element, index) {
+  return (index % 2 === 1);
+});
+
+for (i=0; i < headsArray.length; i++) {
+  headsNL[i].textContent = headsArray[i];
+  textNL[i].textContent = textArray[i];
+};
+
+let midImg = document.getElementById("middle-img");
+midImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+
+//rebuild contact section and footer
+
+let contactStuff = Object.values(siteContent["contact"]);
+
+const contactHead = document.querySelector(".contact h4");
+contactHead.textContent = (contactStuff[0]);
+contactStuff.shift();
+
+let contactNL = document.querySelectorAll(".contact p");
+
+for (i=0; i <contactStuff.length; i++) {
+  contactNL[i].textContent = contactStuff[i];
+}
+
+
+const footText = document.querySelector("footer");
+footText.textContent = (siteContent["footer"]["copyright"]);
